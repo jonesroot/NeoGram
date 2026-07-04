ad8b1baf90cca27ace7bd342bd7ed04ed5a1efe5
jonesroot <ikyodeos378@gmail.com>
2026-07-04 21:05:06 +0700

    feat: Add WriteAccessAllowed class to handle user permissions for bot message writing

--- pyrogram/types/messages_and_media/__init__.py
+++ pyrogram/types/messages_and_media/__init__.py
@@ -62,6 +62,7 @@ from .web_page import WebPage
 from .message_reactions import MessageReactions
 from .message_story import MessageStory
 from .my_boost import MyBoost
+from .write_access_allowed import WriteAccessAllowed
 
 __all__ = [
     "Animation", "Audio", "AvailableEffect", "BoostsStatus", "Contact", "MessageAutoDeleteTimerChanged", "ContactRegistered", "DirectMessagesTopic", "Document", "ExternalReplyInfo", "ForumTopic", "ForumTopicCreated",
@@ -69,5 +70,5 @@ __all__ = [
     "GeneralTopicUnhidden", "Game", "Giveaway", "Location", "Message", "MessageEntity", "Photo", "Thumbnail",
     "StrippedThumbnail", "Story", "StoryDeleted", "StorySkipped", "StoryViews", "TextQuote", "Poll", "PollOption", "Sticker",
     "StickerSet", "Venue", "Video", "VideoQuality", "VideoNote", "Voice", "WebPage", "Dice", "Reaction", "WebAppData",
-    "MessageReactions", "MessageStory", "MyBoost"
+    "MessageReactions", "MessageStory", "MyBoost", "WriteAccessAllowed"
 ]
