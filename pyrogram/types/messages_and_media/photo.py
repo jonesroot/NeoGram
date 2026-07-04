710243e5da8a05d8aad5c714080c0adf772a22fb
jonesroot <ikyodeos378@gmail.com>
2026-07-04 17:11:24 +0700

    feat: enhance message retrieval and origin handling with new parameters and classes

--- pyrogram/types/messages_and_media/photo.py
+++ pyrogram/types/messages_and_media/photo.py
@@ -16,8 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 from datetime import datetime
-from typing import List
 
 import pyrogram
 from pyrogram import raw, utils
@@ -25,26 +25,15 @@ from pyrogram import types
 from pyrogram.file_id import FileId, FileType, FileUniqueId, FileUniqueType, ThumbnailSource
 from ..object import Object
 
+log = logging.getLogger(__name__)
+
 
 class Photo(Object):
     """A Photo.
 
     Parameters:
-        file_id (``str``):
-            Identifier for this file, which can be used to download or reuse the file.
-
-        file_unique_id (``str``):
-            Unique identifier for this file, which is supposed to be the same over time and for different accounts.
-            Can't be used to download or reuse the file.
-
-        width (``int``):
-            Photo width.
-
-        height (``int``):
-            Photo height.
-
-        file_size (``int``):
-            File size.
+        sizes (List of :obj:`~pyrogram.types.Thumbnail`):
+            Available variants of the photo, in different sizes.
 
         date (:py:obj:`~datetime.datetime`):
             Date the photo was sent.
@@ -54,36 +43,36 @@ class Photo(Object):
 
         thumbs (List of :obj:`~pyrogram.types.Thumbnail`, *optional*):
             Available thumbnails of this photo.
+
     """
 
     def __init__(
         self,
         *,
         client: "pyrogram.Client" = None,
-        file_id: str,
-        file_unique_id: str,
-        width: int,
-        height: int,
-        file_size: int,
+        sizes: list["types.Thumbnail"],
         date: datetime,
         ttl_seconds: int = None,
-        thumbs: List["types.Thumbnail"] = None
+        has_spoiler: bool = None,
+        thumbs: list["types.Thumbnail"] = None
     ):
         super().__init__(client)
 
-        self.file_id = file_id
-        self.file_unique_id = file_unique_id
-        self.width = width
-        self.height = height
-        self.file_size = file_size
+        self.sizes = sizes
         self.date = date
         self.ttl_seconds = ttl_seconds
+        self.has_spoiler = has_spoiler
         self.thumbs = thumbs
 
     @staticmethod
-    def _parse(client, photo: "raw.types.Photo", ttl_seconds: int = None) -> "Photo":
+    def _parse(
+        client,
+        photo: "raw.types.Photo",
+        ttl_seconds: int = None,
+        has_spoiler: bool = None,
+    ) -> "Photo":
         if isinstance(photo, raw.types.Photo):
-            photos: List[raw.types.PhotoSize] = []
+            photos: list[raw.types.PhotoSize] = []
 
             for p in photo.sizes:
                 if isinstance(p, raw.types.PhotoSize):
@@ -101,30 +90,84 @@ class Photo(Object):
 
             photos.sort(key=lambda p: p.size)
 
-            main = photos[-1]
-
             return Photo(
-                file_id=FileId(
-                    file_type=FileType.PHOTO,
-                    dc_id=photo.dc_id,
-                    media_id=photo.id,
-                    access_hash=photo.access_hash,
-                    file_reference=photo.file_reference,
-                    thumbnail_source=ThumbnailSource.THUMBNAIL,
-                    thumbnail_file_type=FileType.PHOTO,
-                    thumbnail_size=main.type,
-                    volume_id=0,
-                    local_id=0
-                ).encode(),
-                file_unique_id=FileUniqueId(
-                    file_unique_type=FileUniqueType.DOCUMENT,
-                    media_id=photo.id
-                ).encode(),
-                width=main.w,
-                height=main.h,
-                file_size=main.size,
+                sizes=[
+                    types.Thumbnail(
+                        file_id=FileId(
+                            file_type=FileType.PHOTO,
+                            dc_id=photo.dc_id,
+                            media_id=photo.id,
+                            access_hash=photo.access_hash,
+                            file_reference=photo.file_reference,
+                            thumbnail_source=ThumbnailSource.THUMBNAIL,
+                            thumbnail_file_type=FileType.PHOTO,
+                            thumbnail_size=main.type,
+                            volume_id=0,
+                            local_id=0
+                        ).encode(),
+                        file_unique_id=FileUniqueId(
+                            file_unique_type=FileUniqueType.DOCUMENT,
+                            media_id=photo.id
+                        ).encode(),
+                        width=main.w,
+                        height=main.h,
+                        file_size=main.size,
+                    )
+                    for main in photos
+                ],
                 date=utils.timestamp_to_datetime(photo.date),
                 ttl_seconds=ttl_seconds,
+                has_spoiler=has_spoiler,
                 thumbs=types.Thumbnail._parse(client, photo),
                 client=client
             )
+
+    @property
+    def file_id(self) -> str:
+        log.warning(
+            "This property is deprecated. "
+            "Please use sizes instead"
+        )
+        if len(self.sizes) > 0:
+            return self.sizes[-1].file_id
+        return None
+
+    @property
+    def file_unique_id(self) -> str:
+        log.warning(
+            "This property is deprecated. "
+            "Please use sizes instead"
+        )
+        if len(self.sizes) > 0:
+            return self.sizes[-1].file_unique_id
+        return None
+
+    @property
+    def width(self) -> int:
+        log.warning(
+            "This property is deprecated. "
+            "Please use sizes instead"
+        )
+        if len(self.sizes) > 0:
+            return self.sizes[-1].width
+        return None
+
+    @property
+    def height(self) -> int:
+        log.warning(
+            "This property is deprecated. "
+            "Please use sizes instead"
+        )
+        if len(self.sizes) > 0:
+            return self.sizes[-1].height
+        return None
+
+    @property
+    def file_size(self) -> int:
+        log.warning(
+            "This property is deprecated. "
+            "Please use sizes instead"
+        )
+        if len(self.sizes) > 0:
+            return self.sizes[-1].file_size
+        return None
